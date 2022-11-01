de7da8d83dbf22bc84ec34cbb60233313f896b66
Phalcin <deheroe5556@gmail.com>
2022-11-01 20:34:38 +0000

    update

---
+++ javascript practice/script.js
@@ -0,0 +1,47 @@
+// function hoursIntoSeconds(hour) {
+//   return hour * 60 * 60;
+// }
+
+// console.log(hoursIntoSeconds(2));
+
+// function peri(length, width) {
+//   return 2 * length + 2 * width;
+// }
+
+// console.log(peri(6, 7));
+
+// function calcTriangleArea(base, height) {
+//   return 0.5 * base * height;
+// }
+
+// console.log(calcTriangleArea(10, 10));
+
+// function addFrontend(frontend) {
+//   return frontend + " Frontend";
+// }
+
+// console.log(addFrontend("Apple"));
+
+// function sumGreaterThan100(a, b) {
+//   return a + b >= 100;
+// }
+
+// console.log(sumGreaterThan100(50, 60));
+
+// function lessThanOrEqualToZero(number) {
+//   return number <= 0;
+// }
+
+// console.log(lessThanOrEqualToZero(3));
+
+// function oppositeBoolean(boolean) {
+//   return boolean ? false : true;
+// }
+
+// console.log(oppositeBoolean(true));
+
+function notZero(a) {
+  return a !== 0;
+}
+
+console.log(notZero(5));
