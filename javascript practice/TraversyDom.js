f7dcbf35add7765c8c0b5834195b34708dde2c45
Phalcin <deheroe5556@gmail.com>
2022-11-17 13:22:43 +0000

    update

---
+++ javascript practice/TraversyDom.js
@@ -0,0 +1,38 @@
+// Traversy media Dom
+
+let val;
+
+val = document;
+val = document.all;
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.domain;
+val = document.characterSet;
+
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[0].getAttribute("src");
+
+// Turn node list into an Array
+
+// let scripts = document.scripts;
+// let scriptsArr = Array.from(scripts);
+// scriptsArr.forEach((script) => {
+//   console.log(script.getAttribute("src"));
+// });
+
+// console.log(val);
+
+// DOM SELECTORS
