77e0cd6e7dbb20b9b2d701d429f6821b9c1a8556
Phalcin <deheroe5556@gmail.com>
2022-11-18 13:36:40 +0000

    update

--- javascript practice/TraversyDom.js
+++ javascript practice/TraversyDom.js
@@ -1,83 +1,83 @@
-// Traversy media Dom
+// // Traversy media Dom
 
-let val;
+// let val;
 
-val = document;
-val = document.all;
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.domain;
-val = document.characterSet;
+// val = document;
+// val = document.all;
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.domain;
+// val = document.characterSet;
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].method;
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
+// val = document.links;
+// val = document.links[0];
+// val = document.links[0].id;
+// val = document.links[0].className;
 
-val = document.images;
+// val = document.images;
 
-val = document.scripts;
-val = document.scripts[0].getAttribute("src");
+// val = document.scripts;
+// val = document.scripts[0].getAttribute("src");
 
-// Turn node list into an Array
+// // Turn node list into an Array
 
-// let scripts = document.scripts;
-// let scriptsArr = Array.from(scripts);
-// scriptsArr.forEach((script) => {
-//   console.log(script.getAttribute("src"));
-// });
+// // let scripts = document.scripts;
+// // let scriptsArr = Array.from(scripts);
+// // scriptsArr.forEach((script) => {
+// //   console.log(script.getAttribute("src"));
+// // });
 
-// console.log(val);
+// // console.log(val);
 
-// DOM SELECTORS
-//document.getElementById()
+// // DOM SELECTORS
+// //document.getElementById()
 
-// Get things from the element
-// console.log(document.getElementById("task-title").id);
+// // Get things from the element
+// // console.log(document.getElementById("task-title").id);
 
-const taskTitle = document.getElementById("task-title");
+// const taskTitle = document.getElementById("task-title");
 
-// Change styling
-taskTitle.style.background = "#333";
-taskTitle.style.color = "#fff";
-taskTitle.style.padding = "5px";
-// taskTitle.style.display = "none";
+// // Change styling
+// taskTitle.style.background = "#333";
+// taskTitle.style.color = "#fff";
+// taskTitle.style.padding = "5px";
+// // taskTitle.style.display = "none";
 
-// Change content
-taskTitle.textContent = "Task List";
-taskTitle.innerText = "My tasks";
+// // Change content
+// taskTitle.textContent = "Task List";
+// taskTitle.innerText = "My tasks";
 
-// Add HTML to code
+// // Add HTML to code
 
-taskTitle.innerHTML = '<span style="color:red"> Task List </span>';
+// taskTitle.innerHTML = '<span style="color:red"> Task List </span>';
 
-// Document.querySelector
+// // Document.querySelector
 
-console.log(document.querySelector("#task-title"));
-console.log(document.querySelector(".card-title"));
-console.log(document.querySelector("h5"));
+// console.log(document.querySelector("#task-title"));
+// console.log(document.querySelector(".card-title"));
+// console.log(document.querySelector("h5"));
 
-document.querySelector("li").style.color = "red";
-document.querySelector("ul li").style.color = "blue";
-document.querySelector("li:last-child").style.color = "red";
+// document.querySelector("li").style.color = "red";
+// document.querySelector("ul li").style.color = "blue";
+// document.querySelector("li:last-child").style.color = "red";
 
-document.querySelector("li:nth-child(3)").style.color = "yellow";
+// document.querySelector("li:nth-child(3)").style.color = "yellow";
 
-// document.getElementsByClassName
+// // document.getElementsByClassName
 
-const items = document.getElementsByClassName("collection-item");
-console.log(items);
-console.log(items[0]);
-items[0].style.color = "red";
-items[3].textContent = "Hello";
+// const items = document.getElementsByClassName("collection-item");
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = "red";
+// items[3].textContent = "Hello";
 
-const listItems = document
-  .querySelector("ul")
-  .getElementsByClassName("collection-item");
-console.log(listItems);
+// const listItems = document
+//   .querySelector("ul")
+//   .getElementsByClassName("collection-item");
+// console.log(listItems);
