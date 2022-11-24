50e3f9ce4b82fc3aba9fc38a4ea218bdfe37b4e5
Phalcin <deheroe5556@gmail.com>
2022-11-24 04:08:56 +0000

    update

--- playground.js
+++ playground.js
@@ -1,15 +1,7 @@
-const person = {
-  firstName: "Brad",
-  "this is a key with spaces": true,
-};
+let x = 1;
+let y = x;
 
-person.dog = { name: "Fluffy", age: 2 };
-person.age = 25;
+x = 2;
 
-console.log(person.firstName);
-console.log(person);
-
-// SQUARE BRACKET NOTATION
-const property = "age";
-
-console.log(person["this is a key with spaces"]);
+console.log(x);
+console.log(y);
