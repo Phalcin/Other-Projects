6565a29612b73140b7fc0818b0235a0148df0968
Phalcin <deheroe5556@gmail.com>
2022-12-10 11:59:15 +0000

    update

--- playground.js
+++ playground.js
@@ -1,13 +1,1 @@
-const cars = {
-  car1: "Bugatti",
-  car2: "Ferrari",
-  otherStuff: {
-    house: 1,
-    well: 1,
-  },
-};
-
-console.log(cars);
-const carShow = cars;
-
-JSON.parse(JSON.stringify(carShow));
+console.log("Hello world");
